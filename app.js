41acf516948f8536ba18393c35ac3bd8a8236227
Lokesh Choudhary <lokeshchoudhary8949@gmail.com>
2021-08-16 22:30:08 +0530

    feat: Initial commit
    Working express server application

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const createError = require('http-errors');
+
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World 😀');
+});
+
+app.use(async (req, res, next) => {
+  const error = createError.NotFound();
+  next(error);
+});
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+app.listen(PORT, () => console.log(`Running Server On Port ${PORT} 🚀`));
